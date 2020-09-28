983e52e1ae9afa3b074e336aa55eaf83701732b1
Will Barnes <will.t.barnes@gmail.com>
2020-09-28 15:00:27 -0400

    play around with import order

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,11 +1,14 @@
 import os
 import sys
 
-import sunpy
-
 # Force OpenBLAS to use a single thread to prevent image-rotation
 # test failures on macOS. See https://github.com/sunpy/sunpy/issues/4290
 # for more information.
 os.environ['OMP_NUM_THREADS'] = '1'
+
+import sunpy
+import numpy as np
+
+print(os.environ['OMP_NUM_THREADS'])
 
 sys.exit(sunpy.self_test())
