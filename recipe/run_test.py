8f2d37dbc7f3749e0154ae269f7b4fdfbbf731a8
Stuart Mumford <stuart@cadair.com>
2024-11-27 16:57:38 +0000

    Only run io tests on emulated arch

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import platform
 
 # Force OpenBLAS to use a single thread to prevent image-rotation
 # test failures on macOS. See https://github.com/sunpy/sunpy/issues/4290
@@ -7,5 +8,9 @@ import sys
 os.environ['OMP_NUM_THREADS'] = '1'
 
 import sunpy
+
+if platform.machine() in ('aarch64', 'ppc64le'):
+    print('WARNING: Skipping most tests on aarch64/ppc64le because they take too long')
+    sys.exit(sunpy.self_test("io"))
 
 sys.exit(sunpy.self_test())
