2fb312b1dd26b0e9d8f360a1e0c1bc9bc746aa67
Nabil Freij <nabil.freij@gmail.com>
2025-07-31 14:27:24 -0700

    Update run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -16,7 +16,7 @@ from astropy.utils.iers import conf
 conf.auto_max_age = None
 
 if platform.machine() in ('aarch64', 'ppc64le'):
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "spiceypy"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "spiceypy"])
     print('WARNING: Skipping most tests on aarch64/ppc64le because they take too long')
     sys.exit(sunpy.self_test(package="io"))
 
