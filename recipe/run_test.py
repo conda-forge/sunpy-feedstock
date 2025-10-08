6b1b681abaec102dd081142cd4db701f29fdfb6b
Stuart Mumford <stuart@cadair.com>
2025-10-08 16:12:18 +0100

    This was horrific wtf

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,8 +1,6 @@
 import os
 import sys
-import warnings
 import platform
-import subprocess
 
 # Force OpenBLAS to use a single thread to prevent image-rotation
 # test failures on macOS. See https://github.com/sunpy/sunpy/issues/4290
@@ -16,8 +14,8 @@ from astropy.utils.iers import conf
 conf.auto_max_age = None
 
 if platform.machine() in ('aarch64', 'ppc64le'):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "spiceypy"])
-    print('WARNING: Skipping most tests on aarch64/ppc64le because they take too long')
-    sys.exit(sunpy.self_test(package="io"))
+    print('WARNING: Skipping tests on aarch64/ppc64le because they take too long')
+    sys.exit()
+    # sys.exit(sunpy.self_test(package="io"))
 
 sys.exit(sunpy.self_test())
